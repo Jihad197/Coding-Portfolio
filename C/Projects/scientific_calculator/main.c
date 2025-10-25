4c752875f5d4cde70de35cc3a9de40db45f3f45a
Jihad197 <s231072197@ru.ac.bd>
2025-10-25 15:19:55 +0600

    update normal mode

--- C/Projects/scientific_calculator/main.c
+++ C/Projects/scientific_calculator/main.c
@@ -4,7 +4,8 @@
 int main() {
     int mode;  
     int choice;
-    double a, b, result;
+    float result, num;
+    char op;
 
     printf("===== Welcome =====\n");
     printf("Please select any mode:\n");
@@ -16,9 +17,46 @@ int main() {
     
     // NORMAL MODE
    
-    if (mode == 1) {
+     if (mode == 1) {
         printf("\n--- Normal Mode ---\n");
-        
+        printf("Available operators are +,-,*,/,%%(for percentage),=(for answer)\n");
+        printf("Enter number: \n");
+        scanf("%f",&result);
+
+
+        while(1)
+        {
+            printf("enter an operator[+,-,*,/,%%,=]: ");
+            scanf(" %c",&op);
+            if (op == '=')
+                break;
+
+            printf("Enter next number:\n");
+            scanf("%f",&num);
+            if (op == '+')
+                result = result + num;
+            else if (op == '-')
+            result -= num;
+            else if (op == '*')
+            result *= num;
+            else if (op == '/') {
+                if (num != 0)
+                result /= num;
+                else
+                printf("Error: Division by zero is not possible \n");
+            }
+            else if (op == '%')
+                result = (result * num)/100.0;
+            else
+                printf("Invalid operator\n");
+
+
+
+
+        }
+        printf("\nAnswer = %f\n",result);
+
+
     }
 
     // SCIENTIFIC MODE
